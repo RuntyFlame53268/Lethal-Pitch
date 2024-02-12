d7a61c566791f23b9a33e55ed905483481ce6d60
RuntyFlame53268 <alt1yeet@gmail.com>
2024-02-13 10:37:07 +1100

    create project and packages

---
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LethalCompanyTemplate
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "Lethal ComPitch";
+        public const string PLUGIN_NAME = "Lethal ComPitch";
+        public const string PLUGIN_VERSION = "0.9.0";
+    }
+}
